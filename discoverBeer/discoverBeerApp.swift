e9012ab47bb6adb02e202da06863e161e35753bd
Red <seryprk@gmail.com>
2023-01-03 20:17:23 +0900

    Initial Commit

---
+++ discoverBeer/discoverBeerApp.swift
@@ -0,0 +1,17 @@
+//
+//  discoverBeerApp.swift
+//  discoverBeer
+//
+//  Created by 박세리 on 2023/01/03.
+//
+
+import SwiftUI
+
+@main
+struct discoverBeerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

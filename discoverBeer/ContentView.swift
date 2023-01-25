e2d6e5a9e14f70e24bfb78e013c810fe28801414
Red <seryprk@gmail.com>
2023-01-25 18:19:56 +0900

    ✨feat: clean up code and make image

--- discoverBeer/ContentView.swift
+++ discoverBeer/ContentView.swift
@@ -16,15 +16,21 @@ struct ContentView: View {
             Text("BEER LIST")
                 .font(.title)
                 .fontWeight(.black)
-            VStack(alignment: .leading) {
-                if beers.count == 0 {
-                    Text("데이터를 받아 오지 못했습니다.")
-                } else {
-                    ForEach(beers) { beer in
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 4)
-                                .fill(.brown.opacity(0.4))
-                            Text("\(beer.name)")
+            ScrollView {
+                VStack(alignment: .leading) {
+                    if beers.count == 0 {
+                        Text("데이터를 받아 오지 못했습니다.")
+                    } else {
+                        ForEach(beers) { beer in
+                            ZStack(alignment: .leading) {
+                                RoundedRectangle(cornerRadius: 4)
+                                    .fill(.brown.opacity(0.4))
+                                HStack {
+                                    Image(systemName: "swift")
+                                        .frame(width: 100, height: 100)
+                                    Text("\(beer.name)")
+                                }
+                            }
                         }
                     }
                 }
@@ -36,14 +42,6 @@ struct ContentView: View {
                 guard let data = data else { return }
                 beers = data
             }
-
-            
-//            fetchData { (data, error) in
-//                guard let data = data else { return }
-//                let decoder = JSONDecoder()
-//                let result = try? decoder.decode([BeerElement].self, from: data)
-//                beers = result ?? []
-//            }
         }
     }
     
@@ -63,51 +61,6 @@ struct ContentView: View {
                     completionHandler(nil, error)
                 }
             }
-
-//            .response { response in
-//
-//                switch response.result {
-//                case .success(let data):
-//                    completionHandler(data, nil)
-//                    return
-//                case .failure(let error):
-//                    print(error.localizedDescription)
-//                    completionHandler(nil, error)
-//                }
-    }
-    
-    func fetchData(completionHandler: @escaping (Data?, Error?) -> Void) {
-        // urlRequest 생성
-        let apiurl = "https://api.punkapi.com/v2/beers?page=1&per_page=10"
-//        let apiurl = "https://api.punkapi"
-        guard let url = URL(string: apiurl) else { return }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "GET"
-        
-        // 데이터 통신 코드
-        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            if let error = error {
-                print("😗error form URLSession : \(error)")
-                print("😗response : \(response)")
-                print("😗data : \(data)")
-                completionHandler(nil, error)
-            }
-            
-            if let data = data,
-                let httpResponse = response as? HTTPURLResponse,
-                httpResponse.statusCode == 200 {
-                print("httpResponse: \(httpResponse.statusCode)")
-                print("😗error form URLSession : \(error)")
-                print("😗response : \(response)")
-                print("😗data : \(data)")
-                DispatchQueue.main.async {
-                    completionHandler(data, nil)
-                }
-            } else {
-                completionHandler(nil, error)
-            }
-        }
-        task.resume()
     }
 }
 

101103c9f63afaa3ec1a08e7c4775f00a3eedefa
Red <seryprk@gmail.com>
2023-01-25 15:47:47 +0900

    ✨feat: add data api

--- discoverBeer/ContentView.swift
+++ discoverBeer/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var beers: Beers = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("BEER LIST")
+                .font(.title)
+                .fontWeight(.black)
+            VStack(alignment: .leading) {
+                if beers.count == 0 {
+                    Text("데이터를 받아 오지 못했습니다.")
+                } else {
+                    ForEach(beers) { beer in
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 4)
+                                .fill(.brown.opacity(0.4))
+                            Text("\(beer.name)")
+                        }
+                    }
+                }
+            }
         }
-        .padding()
+        .padding(10)
+        .onAppear {
+            fetchData { (data, error) in
+                guard let data = data else { return }
+                let decoder = JSONDecoder()
+                let result = try? decoder.decode([BeerElement].self, from: data)
+                beers = result ?? []
+            }
+        }
+    }
+    
+    func fetchData(completionHandler: @escaping (Data?, Error?) -> Void) {
+        // urlRequest 생성
+        let apiurl = "https://api.punkapi.com/v2/beers?page=1&per_page=10"
+//        let apiurl = "https://api.punkapi"
+        guard let url = URL(string: apiurl) else { return }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        
+        // 데이터 통신 코드
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                print("😗error form URLSession : \(error)")
+                print("😗response : \(response)")
+                print("😗data : \(data)")
+                completionHandler(nil, error)
+            }
+            
+            if let data = data,
+                let httpResponse = response as? HTTPURLResponse,
+                httpResponse.statusCode == 200 {
+                print("httpResponse: \(httpResponse.statusCode)")
+                print("😗error form URLSession : \(error)")
+                print("😗response : \(response)")
+                print("😗data : \(data)")
+                DispatchQueue.main.async {
+                    completionHandler(data, nil)
+                }
+            } else {
+                completionHandler(nil, error)
+            }
+        }
+        task.resume()
     }
 }
 
